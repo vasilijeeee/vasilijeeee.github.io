dfcdcf7ceee58e2e34304f4af26657eaca40e0c8
vasilijeeee <134314894+vasilijeeee@users.noreply.github.com>
2023-05-22 19:43:58 +0200

    Delete angular-company-website-master directory

--- angular-company-website-master/src/app/core/_model/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './navigation';
-export * from './page';
-export * from './preset';
-export * from './external-link';
--- angular-company-website-master/src/app/shared/_pipes/trust-cosmic.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrustCosmicPipe } from './trust-cosmic.pipe';
-
-describe('TrustCosmicPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrustCosmicPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
